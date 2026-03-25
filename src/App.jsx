21f7ddc8aa7a4c4e7f65110b41e01f1b6de84a5f
Yuri Machado <yurimachadoluz@gmail.com>
2026-03-25 19:52:14 -0300

    add: aviso

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { AnimatedBackground } from "@components";
 import { About, Contact, Footer, Hero, Navbar, Projects } from "@layout";
 import "@styles/index.css";
+import Warning from "@components/warning";
+
 
 function App() {
   return (
     <>
+      <Warning />
       <AnimatedBackground />
       <header className="flex w-full justify-center pt-6">
         <Navbar />
